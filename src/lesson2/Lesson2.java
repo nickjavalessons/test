d2f9b4fcdd2323faec4c09b2871aae8677344d65
Nickolay <biblbrox@yandex.ru>
2019-03-11 20:44:52 +0300

    first commit

---
+++ src/lesson2/Lesson2.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int code = scan.nextInt();
+        outer: for(int i = 0; i < 3; i++){
+
+        }
+    }
+
+
+}
